eee0cd5d9fb4beed3c613be54d1cff865ac56369
Angela Sánchez <angela.san.mx@gmail.com>
2022-02-18 23:27:04 -0600

    Water_Left, Config.py, Open.py
    Manejo de errores.
    Necesito revisar el primer ejercicio para llegar a los errores solicitados.

---
+++ 10 Manejo  de errores/Config.py
@@ -0,0 +1,17 @@
+def main():
+    try:
+        configuration = open('config.txt')
+    except FileNotFoundError:
+        print("Couldn't find the config.txt file!")
+
+
+if __name__ == '__main__':
+    main()
+
+
+
+
+
+
+
+
---
+++ 10 Manejo  de errores/Config.txt/Config.py
@@ -0,0 +1,18 @@
+def main():
+    try:
+        configuration = open('config.txt')
+    except FileNotFoundError:
+        print("Couldn't find the config.txt file!")
+
+
+if __name__ == '__main__':
+    main()
+
+
+$ python config.py
+Traceback (most recent call last):
+  File "/tmp/config.py", line 9, in <module>
+    main()
+  File "/tmp/config.py", line 3, in main
+    configuration = open('config.txt')
+IsADirectoryError: [Errno 21] Is a directory: 'config.txt'
